8f32eae7296b5b23cc480d1dd3947b2cd3fdab34
Yogesh Singh <ayogesh1214@gmail.com>
2025-07-30 11:39:30 +0530

    draft commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // 🚨 disables TypeScript errors during build
+  },
+};
+
+export default nextConfig;
